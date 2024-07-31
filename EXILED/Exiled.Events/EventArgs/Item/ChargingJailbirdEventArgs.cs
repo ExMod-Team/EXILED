1fe545d3613ceb2790f2fbb718471222e0632138
Yamato <louismonneyron5@yahoo.com>
2024-08-01 00:19:16 +0200

    Jailbird

--- EXILED/Exiled.Events/EventArgs/Item/ChargingJailbirdEventArgs.cs
+++ EXILED/Exiled.Events/EventArgs/Item/ChargingJailbirdEventArgs.cs
@@ -25,7 +25,7 @@ namespace Exiled.Events.EventArgs.Item
         public ChargingJailbirdEventArgs(ReferenceHub player, InventorySystem.Items.ItemBase swingItem, bool isAllowed = true)
         {
             Player = Player.Get(player);
-            Item = Item.Get(swingItem);
+            Jailbird = (Jailbird)Item.Get(swingItem);
             IsAllowed = isAllowed;
         }
 
@@ -35,9 +35,14 @@ namespace Exiled.Events.EventArgs.Item
         public Player Player { get; }
 
         /// <summary>
-        /// Gets the <see cref="API.Features.Items.Item"/> that is being charged. This will always be a <see cref="Jailbird"/>.
+        /// Gets the <see cref="API.Features.Items.Jailbird"/> that is being charged.
         /// </summary>
-        public Item Item { get; }
+        public Jailbird Jailbird { get; }
+
+        /// <summary>
+        /// Gets the <see cref="API.Features.Items.Item"/> that is being charged.
+        /// </summary>
+        public Item Item => Jailbird;
 
         /// <summary>
         /// Gets or sets a value indicating whether or not the Jailbird can be charged.
