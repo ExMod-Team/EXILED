608bbcd6d36cb80bc8facba3b5a54986181566d6
Yamato <louismonneyron5@yahoo.com>
2024-08-01 23:54:06 +0200

    Fix Naming

--- EXILED/Exiled.Events/EventArgs/Scp079/RecontainedEventArgs.cs
+++ EXILED/Exiled.Events/EventArgs/Scp079/RecontainedEventArgs.cs
@@ -23,14 +23,14 @@ namespace Exiled.Events.EventArgs.Scp079
         /// <inheritdoc cref="Player" />
         /// </param>
         /// <param name="scp079Recontainer">
-        /// <inheritdoc cref="Scp079Recontainer" />
+        /// <inheritdoc cref="Recontainer" />
         /// </param>
         public RecontainedEventArgs(Player player, PlayerRoles.PlayableScps.Scp079.Scp079Recontainer scp079Recontainer)
         {
             Player = player;
             Scp079 = player.Role.As<Scp079Role>();
-            Scp079Recontainer = scp079Recontainer;
-            PlayerWhoRecontainedScp079DoNotKeepThisNAmeOKAYYYY = Player.Get(scp079Recontainer._activatorGlass.LastAttacker);
+            Recontainer = scp079Recontainer;
+            Attacker = Player.Get(scp079Recontainer._activatorGlass.LastAttacker);
             IsAutomatic = scp079Recontainer._activatorGlass.LastAttacker.IsSet;
         }
 
@@ -45,12 +45,12 @@ namespace Exiled.Events.EventArgs.Scp079
         /// <summary>
         /// Gets the instance that handle SCP-079 recontained proccess.
         /// </summary>
-        public PlayerRoles.PlayableScps.Scp079.Scp079Recontainer Scp079Recontainer { get; }
+        public PlayerRoles.PlayableScps.Scp079.Scp079Recontainer Recontainer { get; }
 
         /// <summary>
         /// Gets the player that recontained SCP-079.
         /// </summary>
-        public Player PlayerWhoRecontainedScp079DoNotKeepThisNAmeOKAYYYY { get; }
+        public Player Attacker { get; }
 
         /// <summary>
         /// Gets a value indicating whether or not the recontained has been made automatically or by triggering the proccess.
