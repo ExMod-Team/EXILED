14ee8e59cd1548e28fed072a6a2e9e1a78f5d1eb
6hundred9 <96420090+6hundred9@users.noreply.github.com>
2024-08-28 20:31:37 +0300

    build error

--- EXILED/Exiled.Events/Patches/Events/Scp173/BeingObserved.cs
+++ EXILED/Exiled.Events/Patches/Events/Scp173/BeingObserved.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="Scp173BeingObserved.cs" company="Exiled Team">
+// <copyright file="BeingObserved.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
