6edfe9b1491d410b9c74f33d499c4c5ba07c95a3
TtroubleTT <TtroubleTT2@gmail.com>
2024-10-25 16:55:27 -0700

    Fix NPC's not being seen in the RA if you don't use dedicated server mode

--- EXILED/Exiled.API/Features/Npc.cs
+++ EXILED/Exiled.API/Features/Npc.cs
@@ -173,7 +173,7 @@ namespace Exiled.API.Features
                 }
                 else
                 {
-                    npc.ReferenceHub.authManager.InstanceMode = ClientInstanceMode.Unverified;
+                    npc.ReferenceHub.authManager.InstanceMode = ClientInstanceMode.Host;
                     npc.ReferenceHub.authManager._privUserId = userId == string.Empty ? $"Dummy@localhost" : userId;
                 }
             }
