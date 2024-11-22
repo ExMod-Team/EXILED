747d2fff6843f2e87b1d7464776efed4a8f03a15
Yamato <66829532+louis1706@users.noreply.github.com>
2024-11-22 18:52:11 +0100

    Update EXILED/Exiled.API/Features/BanManager.cs

--- EXILED/Exiled.API/Features/BanManager.cs
+++ EXILED/Exiled.API/Features/BanManager.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="BanManager.cs" company="Exiled Team">
+// <copyright file="BanManager.cs" company="ExMod Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
