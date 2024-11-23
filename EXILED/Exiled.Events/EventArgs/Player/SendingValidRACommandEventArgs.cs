31af95140614c627265aa0dc86ecb3cade996438
Mariki <68015763+Mar1ki@users.noreply.github.com>
2024-11-23 10:36:31 +0300

    Update SendingValidRACommandEventArgs.cs

--- EXILED/Exiled.Events/EventArgs/Player/SendingValidRACommandEventArgs.cs
+++ EXILED/Exiled.Events/EventArgs/Player/SendingValidRACommandEventArgs.cs
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------
-// <copyright file="SendingValidRACommandEventArgs.cs" company="Exiled Team">
-// Copyright (c) Exiled Team. All rights reserved.
+// <copyright file="SendingValidRACommandEventArgs.cs" company="ExMod Team">
+// Copyright (c) ExMod Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
 // -----------------------------------------------------------------------
