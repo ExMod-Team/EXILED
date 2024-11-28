bfc362a9383f8c1eeecfb2960bc84af9929038ac
Bolton <48883340+BoltonDev@users.noreply.github.com>
2024-11-28 19:50:37 +0100

    feat: spawn the speaker

--- EXILED/Exiled.API/Features/Toys/Speaker.cs
+++ EXILED/Exiled.API/Features/Toys/Speaker.cs
@@ -87,12 +87,25 @@ namespace Exiled.API.Features.Toys
         }
 
         /// <summary>
-        /// Creates a <see cref="Speaker"/>.
+        /// Creates a new <see cref="Speaker"/>.
         /// </summary>
-        /// <returns>The <see cref="Speaker"/>.</returns>
-        public static Speaker Create()
+        /// <param name="position">The position of the <see cref="Speaker"/>.</param>
+        /// <param name="rotation">The rotation of the <see cref="Speaker"/>.</param>
+        /// <param name="scale">The scale of the <see cref="Speaker"/>.</param>
+        /// <param name="spawn">Whether the <see cref="Speaker"/> should be initially spawned.</param>
+        /// <returns>The new <see cref="Speaker"/>.</returns>
+        public static Speaker Create(Vector3? position, Vector3? rotation, Vector3? scale, bool spawn)
         {
-            Speaker speaker = new(Object.Instantiate(Prefab));
+            Speaker speaker = new(UnityEngine.Object.Instantiate(Prefab))
+            {
+                Position = position ?? Vector3.zero,
+                Rotation = Quaternion.Euler(rotation ?? Vector3.zero),
+                Scale = scale ?? Vector3.one,
+            };
+
+            if (spawn)
+                speaker.Spawn();
+
             return speaker;
         }
     }
