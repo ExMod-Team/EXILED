f09f7024a7f5b01a70956a80b624c317e1eca17a
TeMbI4 <koshart17@gmail.com>
2024-12-06 11:48:32 +0100

    Fix conflict

--- EXILED/Exiled.Events/EventArgs/Server/EndingRoundEventArgs.cs
+++ EXILED/Exiled.Events/EventArgs/Server/EndingRoundEventArgs.cs
@@ -21,21 +21,18 @@ namespace Exiled.Events.EventArgs.Server
         /// <param name="classList">
         /// <inheritdoc cref="RoundSummary.SumInfo_ClassList" />
         /// </param>
-        /// <param name="leadingTeam">
-        /// <inheritdoc cref="LeadingTeam" />
+        /// <param name="isForceEnded">
+        /// <inheritdoc cref="IsForceEnded" />
         /// </param>
         /// <param name="isAllowed">
         /// <inheritdoc cref="IsAllowed" />
         /// </param>
-        /// <param name="isForceEnded">
-        /// <inheritdoc cref="IsForceEnded" />
-        /// </param>
-        public EndingRoundEventArgs(RoundSummary.LeadingTeam leadingTeam, RoundSummary.SumInfo_ClassList classList, bool isAllowed, bool isForceEnded)
+        public EndingRoundEventArgs(RoundSummary.SumInfo_ClassList classList, bool isForceEnded, bool isAllowed)
         {
             ClassList = classList;
-            LeadingTeam = (LeadingTeam)leadingTeam;
-            IsAllowed = isAllowed;
+            LeadingTeam = GetLeadingTeam(classList);
             IsForceEnded = isForceEnded;
+            IsAllowed = isAllowed;
         }
 
         /// <summary>
@@ -49,13 +46,33 @@ namespace Exiled.Events.EventArgs.Server
         public LeadingTeam LeadingTeam { get; set; }
 
         /// <summary>
-        /// Gets or Sets a value indicating whether the round is ended by API call.
+        /// Gets or sets a value indicating whether the round is ended by API call.
         /// </summary>
         public bool IsForceEnded { get; set; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether the event can be executed.
+        /// Gets or sets a value indicating whether the round is going to finish or not.
         /// </summary>
         public bool IsAllowed { get; set; }
+
+        private LeadingTeam GetLeadingTeam(RoundSummary.SumInfo_ClassList classList)
+        {
+            // NW logic
+            int facilityForces = classList.mtf_and_guards + classList.scientists;
+            int chaosInsurgency = classList.chaos_insurgents + classList.class_ds;
+            int anomalies = classList.scps_except_zombies + classList.zombies;
+            int num4 = facilityForces > 0 ? 1 : 0;
+            bool flag1 = chaosInsurgency > 0;
+            bool flag2 = anomalies > 0;
+            RoundSummary.LeadingTeam leadingTeam = RoundSummary.LeadingTeam.Draw;
+            if (num4 != 0)
+                leadingTeam = RoundSummary.EscapedScientists >= RoundSummary.EscapedClassD ? RoundSummary.LeadingTeam.FacilityForces : RoundSummary.LeadingTeam.Draw;
+            else if (flag2 || flag2 & flag1)
+                leadingTeam = RoundSummary.EscapedClassD > RoundSummary.SurvivingSCPs ? RoundSummary.LeadingTeam.ChaosInsurgency : (RoundSummary.SurvivingSCPs > RoundSummary.EscapedScientists ? RoundSummary.LeadingTeam.Anomalies : RoundSummary.LeadingTeam.Draw);
+            else if (flag1)
+                leadingTeam = RoundSummary.EscapedClassD >= RoundSummary.EscapedScientists ? RoundSummary.LeadingTeam.ChaosInsurgency : RoundSummary.LeadingTeam.Draw;
+
+            return (LeadingTeam)leadingTeam;
+        }
     }
 }
