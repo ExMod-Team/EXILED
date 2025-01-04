88fd08f7c380fe8a28f9c7f58f36916e1345cf3e
Yamato <louismonneyron5@yahoo.com>
2025-01-04 01:00:54 +0100

    replace Mathf.Clamp01 by Mathf.Clamp

--- EXILED/Exiled.Events/Patches/Generic/FixStaminaStat.cs
+++ EXILED/Exiled.Events/Patches/Generic/FixStaminaStat.cs
@@ -1,4 +1,4 @@
-﻿// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
 // <copyright file="FixStaminaStat.cs" company="ExMod Team">
 // Copyright (c) ExMod Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
@@ -7,6 +7,7 @@
 
 namespace Exiled.Events.Patches.Generic
 {
+    using System;
     using System.Collections.Generic;
     using System.Reflection.Emit;
 
@@ -15,6 +16,7 @@ namespace Exiled.Events.Patches.Generic
     using HarmonyLib;
     using PlayerRoles.FirstPersonControl;
     using PlayerStatsSystem;
+    using UnityEngine;
 
     using static HarmonyLib.AccessTools;
 
@@ -28,15 +30,41 @@ namespace Exiled.Events.Patches.Generic
         {
             List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
 
-            int index = newInstructions.FindLastIndex(i => i.opcode == OpCodes.Ldc_R4);
+            // replace "1f" const to "this._stat.MaxValue"
+            int offset = 0;
+            int index = newInstructions.FindLastIndex(i => i.opcode == OpCodes.Ldc_R4 && (float)i.operand == 1f) + offset;
+
             newInstructions.RemoveAt(index);
 
             newInstructions.InsertRange(index, new CodeInstruction[]
             {
+                // this._stat.MaxValue
                 new(OpCodes.Ldarg_0),
                 new(OpCodes.Ldfld, Field(typeof(FpcStateProcessor), nameof(FpcStateProcessor._stat))),
                 new(OpCodes.Callvirt, PropertyGetter(typeof(StaminaStat), nameof(StaminaStat.MaxValue))),
             });
+
+            // replace all "Mathf.Clamp01(value)" to "Mathf.Clamp(value, 0, this._stat.MaxValue)"
+            System.Reflection.MethodInfo clamp01Method = Method(typeof(Mathf), nameof(Mathf.Clamp01));
+            System.Reflection.MethodInfo clampMethod = Method(typeof(Mathf), nameof(Mathf.Clamp), new Type[] { typeof(float), typeof(float), typeof(float), });
+            for (int i = 0; i < newInstructions.Count; i++)
+            {
+                CodeInstruction codeInstruction = newInstructions[i];
+                if (codeInstruction.operand == (object)clamp01Method)
+                {
+                    newInstructions.InsertRange(i, new CodeInstruction[]
+                    {
+                        // 0f
+                        new(OpCodes.Ldc_R4, 0f),
+
+                        // this._stat.MaxValue
+                        new(OpCodes.Ldarg_0),
+                        new(OpCodes.Ldfld, Field(typeof(FpcStateProcessor), nameof(FpcStateProcessor._stat))),
+                        new(OpCodes.Callvirt, PropertyGetter(typeof(StaminaStat), nameof(StaminaStat.MaxValue))),
+                    });
+                    codeInstruction.operand = clampMethod;
+                }
+            }
 
             for (int z = 0; z < newInstructions.Count; z++)
                 yield return newInstructions[z];
