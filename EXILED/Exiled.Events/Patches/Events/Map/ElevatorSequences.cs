c51cb474da5bcf9190298ae9b75ac0466e23627d
Lumi <me@jxtq.moe>
2025-01-09 18:46:30 +0000

    refactor: put elevator patches into 1 patch

--- EXILED/Exiled.Events/Patches/Events/Map/ElevatorSequences.cs
+++ EXILED/Exiled.Events/Patches/Events/Map/ElevatorSequences.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="ElevatorSequencesUpdated.cs" company="ExMod Team">
+// <copyright file="ElevatorSequences.cs" company="ExMod Team">
 // Copyright (c) ExMod Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
@@ -14,11 +14,12 @@ namespace Exiled.Events.Patches.Events.Map
 
     /// <summary>
     /// Patches <see cref="ElevatorChamber.CurSequence" />'s setter.
-    /// Adds the <see cref="Map.ElevatorArrived" /> event.
+    /// Adds the <see cref="Handlers.Map.ElevatorArrived" /> event.
+    /// Adds the <see cref="Handlers.Map.ElevatorSequencesUpdated" /> event.
     /// </summary>
     [EventPatch(typeof(Handlers.Map), nameof(Handlers.Map.ElevatorSequencesUpdated))]
     [HarmonyPatch(typeof(ElevatorChamber), nameof(ElevatorChamber.CurSequence), MethodType.Setter)]
-    internal class ElevatorSequencesUpdated
+    internal class ElevatorSequences
     {
         // ReSharper disable once InconsistentNaming
 #pragma warning disable SA1313
@@ -27,6 +28,10 @@ namespace Exiled.Events.Patches.Events.Map
         {
             ElevatorSequencesUpdatedEventArgs ev = new(__instance, __instance.CurSequence);
             Handlers.Map.OnElevatorSequencesUpdated(ev);
+            if (__instance.CurSequence != ElevatorChamber.ElevatorSequence.DoorOpening)
+                return;
+            ElevatorArrivedEventArgs ev2 = new(__instance, __instance.DestinationLevel);
+            Handlers.Map.OnElevatorArrived(ev2);
         }
     }
 }
