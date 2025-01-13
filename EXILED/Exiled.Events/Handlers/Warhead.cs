2ef7fe9a288dcaa9d08973172369181ecf3a4a40
Yamato <louismonneyron5@yahoo.com>
2025-01-13 17:38:09 +0100

    DeadmanSwitchEventAndAPI

--- EXILED/Exiled.Events/Handlers/Warhead.cs
+++ EXILED/Exiled.Events/Handlers/Warhead.cs
@@ -38,6 +38,11 @@ namespace Exiled.Events.Handlers
         public static Event Detonated { get; set; } = new();
 
         /// <summary>
+        /// Invoked before the DeadmanSwitch is Initiated.
+        /// </summary>
+        public static Event DeadmanSwitchInitiating { get; set; } = new();
+
+        /// <summary>
         /// Invoked before detonating the warhead.
         /// </summary>
         public static Event<DetonatingEventArgs> Detonating { get; set; } = new();
@@ -64,6 +69,11 @@ namespace Exiled.Events.Handlers
         /// Called after the warhead has been detonated.
         /// </summary>
         public static void OnDetonated() => Detonated.InvokeSafely();
+
+        /// <summary>
+        /// Called after the warhead has been detonated.
+        /// </summary>
+        public static void OnDeadmanSwitchInitiating() => DeadmanSwitchInitiating.InvokeSafely();
 
         /// <summary>
         /// Called before detonating the warhead.
