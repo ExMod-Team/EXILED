c6ae353846ee9e90cc095ced1f69b6d4a0cbc6bf
Yamato <louismonneyron5@yahoo.com>
2025-01-19 17:34:29 +0100

    No breaking change

--- EXILED/Exiled.API/Features/Player.cs
+++ EXILED/Exiled.API/Features/Player.cs
@@ -2203,13 +2203,28 @@ namespace Exiled.API.Features
         /// Forces the player to use an item.
         /// </summary>
         /// <param name="usableItem">The ItemType to be used.</param>
-        public void UseItem(ItemType usableItem) => UseItem(Item.Create<Usable>(usableItem));
+        /// <returns><see langword="true"/> if item was used successfully. Otherwise, <see langword="false"/>.</returns>
+        public bool UseItem(ItemType usableItem) => UseItem(Item.Create(usableItem));
+
+        /// <summary>
+        /// Forces the player to use an item.
+        /// </summary>
+        /// <param name="usable">The item to be used.</param>
+        public void UseItem(Usable usable) => usable?.Use(this);
 
         /// <summary>
         /// Forces the player to use an item.
         /// </summary>
         /// <param name="item">The item to be used.</param>
-        public void UseItem(Usable item) => item.Use(this);
+        /// <returns><see langword="true"/> if item was used successfully. Otherwise, <see langword="false"/>.</returns>
+        public bool UseItem(Item item)
+        {
+            if (item is not Usable usableItem)
+                return false;
+
+            UseItem(usableItem);
+            return true;
+        }
 
         /// <summary>
         /// Kills the player.
