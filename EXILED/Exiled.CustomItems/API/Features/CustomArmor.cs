e22b4bd4bb82d3f271987efac15950e0f22eab9d
Yamato <louismonneyron5@yahoo.com>
2025-01-20 12:16:55 +0100

    No need of delay also call the same method twice

--- EXILED/Exiled.CustomItems/API/Features/CustomArmor.cs
+++ EXILED/Exiled.CustomItems/API/Features/CustomArmor.cs
@@ -70,10 +70,7 @@ namespace Exiled.CustomItems.API.Features
 
             TrackedSerials.Add(armor.Serial);
 
-            Timing.CallDelayed(0.05f, () => OnAcquired(player, armor, displayMessage));
-
-            if (displayMessage)
-                ShowPickedUpMessage(player);
+            OnAcquired(player, armor, displayMessage);
         }
 
         /// <inheritdoc/>
