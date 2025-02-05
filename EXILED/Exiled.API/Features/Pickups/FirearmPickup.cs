e04be3817df82aaf5a1a42a106c0402f40e1eeec
Banalny-Banan <elisey1324@gmail.com>
2025-02-05 20:30:56 +0200

    first

--- EXILED/Exiled.API/Features/Pickups/FirearmPickup.cs
+++ EXILED/Exiled.API/Features/Pickups/FirearmPickup.cs
@@ -54,11 +54,6 @@ namespace Exiled.API.Features.Pickups
         public new BaseFirearm Base { get; }
 
         /// <summary>
-        /// Gets a value indicating whether the pickup is already distributed.
-        /// </summary>
-        public bool IsDistributed { get; internal set; }
-
-        /// <summary>
         /// Gets or sets a value indicating how much ammo can contain this <see cref="FirearmPickup"/>.
         /// </summary>
         public int MaxAmmo { get; set; }
@@ -104,7 +99,7 @@ namespace Exiled.API.Features.Pickups
         }
 
         /// <summary>
-        /// Gets or sets a ammo drain per shoot.
+        /// Gets or sets the ammo drain per shoot.
         /// </summary>
         /// <remarks>
         /// Always <see langword="1"/> by default.
@@ -121,19 +116,16 @@ namespace Exiled.API.Features.Pickups
             set => Base.Worldmodel.Setup(Base.CurId, Base.Worldmodel.WorldmodelType, value);
         }
 
-        /// <inheritdoc />
-        public override void Spawn()
-        {
-            base.Spawn();
-            if (!IsDistributed)
-                Base.OnDistributed();
-        }
+        /// <summary>
+        /// Initializes the item as if it was spawned naturally by map generation.
+        /// </summary>
+        public void Distribute() => Base.OnDistributed();
 
         /// <summary>
-        /// Returns the FirearmPickup in a human readable format.
+        /// Returns the FirearmPickup in a human-readable format.
         /// </summary>
         /// <returns>A string containing FirearmPickup related data.</returns>
-        public override string ToString() => $"{Type} ({Serial}) [{Weight}] *{Scale}* |{IsDistributed}| -{/*Ammo*/0}-";
+        public override string ToString() => $"{Type} ({Serial}) [{Weight}] *{Scale}*";
 
         /// <inheritdoc/>
         internal override void ReadItemInfo(Items.Item item)
