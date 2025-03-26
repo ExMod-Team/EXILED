c74ed5913bff3ab20a3023ba5c0f95f1978828b8
Yamato <louismonneyron5@yahoo.com>
2025-03-27 00:11:01 +0100

    Fix build error

--- EXILED/Exiled.Events/EventArgs/Interfaces/IMicroHIDEvent.cs
+++ EXILED/Exiled.Events/EventArgs/Interfaces/IMicroHIDEvent.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="IFirearmEvent.cs" company="ExMod Team">
+// <copyright file="IMicroHIDEvent.cs" company="ExMod Team">
 // Copyright (c) ExMod Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
--- EXILED/Exiled.Events/EventArgs/Player/ExplodingMicroHidEventArgs.cs
+++ EXILED/Exiled.Events/EventArgs/Player/ExplodingMicroHidEventArgs.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="ExplodingMicroHidEventArgs.cs" company="ExMod Team">
+// <copyright file="ExplodingMicroHIDEventArgs.cs" company="ExMod Team">
 // Copyright (c) ExMod Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
