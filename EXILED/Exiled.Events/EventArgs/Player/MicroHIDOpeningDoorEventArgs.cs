b73c000ddc10d73bb083763e07a437937cf48ed6
Mike <146554836+MikeSus1@users.noreply.github.com>
2025-03-27 15:08:17 +0100

    addevent

---
+++ EXILED/Exiled.Events/EventArgs/Player/MicroHIDOpeningDoorEventArgs.cs
@@ -0,0 +1,51 @@
+// -----------------------------------------------------------------------
+// <copyright file="MicroHIDOpeningDoorEventArgs.cs" company="ExMod Team">
+// Copyright (c) ExMod Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.EventArgs.Player
+{
+    using Exiled.API.Features.Items;
+    using Exiled.Events.EventArgs.Interfaces;
+    using InventorySystem.Items.MicroHID;
+
+    /// <summary>
+    /// Contains all information before the micro hid explode.
+    /// </summary>
+    public class MicroHIDOpeningDoorEventArgs : IDeniableEvent, IPlayerEvent, IItemEvent
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MicroHIDOpeningDoorEventArgs"/> class.
+        /// </summary>
+        /// <param name="item"><inheritdoc cref="Item"/></param>
+        /// <param name="isAllowed">Whether the Micro HID can open the door or not.</param>
+        public MicroHIDOpeningDoorEventArgs(MicroHIDItem item, bool isAllowed = true)
+        {
+            MicroHID = Item.Get<MicroHid>(item);
+            Player = MicroHID.Owner;
+            IsAllowed = isAllowed;
+        }
+
+        /// <summary>
+        /// Gets the item.
+        /// </summary>
+        public Item Item => MicroHID;
+
+        /// <summary>
+        /// Gets the player in owner of the item.
+        /// </summary>
+        public Exiled.API.Features.Player Player { get; }
+
+        /// <summary>
+        /// Gets MicroHid item.
+        /// </summary>
+        public MicroHid MicroHID { get; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the player can explode the micro HID.
+        /// </summary>
+        public bool IsAllowed { get; set; }
+    }
+}
---
+++ EXILED/Exiled.Events/Patches/Events/Player/MicroHIDOpeningDoor.cs
@@ -0,0 +1,62 @@
+// -----------------------------------------------------------------------
+// <copyright file="MicroHIDOpeningDoor.cs" company="ExMod Team">
+// Copyright (c) ExMod Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
+
+namespace Exiled.Events.Patches.Events.Player
+{
+    using Attributes;
+
+    using Exiled.Events.EventArgs.Player;
+
+    using HarmonyLib;
+
+    using Interactables;
+    using Interactables.Interobjects;
+    using InventorySystem.Items.MicroHID.Modules;
+
+    /// <summary>
+    /// Patches <see cref="InventorySystem.Items.MicroHID.Modules.ChargeFireModeModule.HandlePotentialDoor"/>.
+    /// Adds the <see cref="MicroHIDOpeningDoor" /> event.
+    /// </summary>
+    [EventPatch(typeof(Handlers.Player), nameof(Handlers.Player.MicroHIDOpeningDoor))]
+    [HarmonyPatch(typeof(ChargeFireModeModule), nameof(ChargeFireModeModule.HandlePotentialDoor))]
+    internal static class MicroHIDOpeningDoor
+    {
+        private static void Prefix(ref ChargeFireModeModule __instance, InteractableCollider interactable)
+        {
+            BreakableDoor breakableDoor = interactable.Target as BreakableDoor;
+            if(breakableDoor == null)
+            {
+                return;
+            }
+
+            if (breakableDoor.TargetState)
+            {
+                return;
+            }
+
+            if (breakableDoor.AllowInteracting(__instance.Item.Owner, interactable.ColliderId))
+            {
+                return;
+            }
+
+            MicroHIDOpeningDoorEventArgs ev = new(__instance.MicroHid);
+            Exiled.Events.Handlers.Player.OnMicroHIDOpeningDoor(ev);
+
+            if (!ev.IsAllowed)
+            {
+                return;
+            }
+
+            if ((breakableDoor.ActiveLocks & (ushort)(~(ushort)ChargeFireModeModule.BypassableLocks)) == 0)
+            {
+                breakableDoor.NetworkTargetState = true;
+            }
+        }
+    }
+}
