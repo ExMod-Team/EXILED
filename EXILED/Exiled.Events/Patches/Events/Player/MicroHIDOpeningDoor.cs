a4f121f98c7fa650e30767fbf158ea5d4302ec63
Yamato <louismonneyron5@yahoo.com>
2025-03-31 12:19:23 +0200

    Remove Prefix code

--- EXILED/Exiled.Events/Patches/Events/Player/MicroHIDOpeningDoor.cs
+++ EXILED/Exiled.Events/Patches/Events/Player/MicroHIDOpeningDoor.cs
@@ -5,19 +5,19 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
-#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
-
 namespace Exiled.Events.Patches.Events.Player
 {
-    using Attributes;
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
 
+    using Attributes;
+    using Exiled.API.Features.Pools;
     using Exiled.Events.EventArgs.Player;
 
     using HarmonyLib;
-
-    using Interactables;
-    using Interactables.Interobjects;
     using InventorySystem.Items.MicroHID.Modules;
+
+    using static HarmonyLib.AccessTools;
 
     /// <summary>
     /// Patches <see cref="InventorySystem.Items.MicroHID.Modules.ChargeFireModeModule.HandlePotentialDoor"/>.
@@ -27,33 +27,47 @@ namespace Exiled.Events.Patches.Events.Player
     [HarmonyPatch(typeof(ChargeFireModeModule), nameof(ChargeFireModeModule.HandlePotentialDoor))]
     internal static class MicroHIDOpeningDoor
     {
-        private static bool Prefix(ref ChargeFireModeModule __instance, InteractableCollider interactable)
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            BreakableDoor breakableDoor = interactable.Target as BreakableDoor;
-            if (breakableDoor == null)
-            {
-                return false;
-            }
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
 
-            if (breakableDoor.TargetState)
-            {
-                return false;
-            }
+            int offset = 0;
+            int index = newInstructions.FindLastIndex(x => x.opcode == OpCodes.Brtrue_S) + offset;
 
-            if (breakableDoor.AllowInteracting(__instance.Item.Owner, interactable.ColliderId))
-            {
-                return false;
-            }
+            LocalBuilder result = generator.DeclareLocal(typeof(bool));
 
-            MicroHIDOpeningDoorEventArgs ev = new(__instance.MicroHid, breakableDoor, (breakableDoor.ActiveLocks & (ushort)~(ushort)ChargeFireModeModule.BypassableLocks) == 0);
-            Exiled.Events.Handlers.Player.OnMicroHIDOpeningDoor(ev);
+            newInstructions.InsertRange(
+                index,
+                new CodeInstruction[]
+                {
+                    // bool result = onStack
+                    new(OpCodes.Stloc_S, result.LocalIndex),
 
-            if (ev.IsAllowed)
-            {
-                breakableDoor.NetworkTargetState = true;
-            }
+                    // this.MicroHid
+                    new(OpCodes.Ldarg_0),
+                    new(OpCodes.Call, PropertyGetter(typeof(ChargeFireModeModule), nameof(ChargeFireModeModule.MicroHid))),
 
-            return false;
+                    // breakableDoor
+                    new(OpCodes.Ldloc_0),
+
+                    // result
+                    new(OpCodes.Ldloc_S, result.LocalIndex),
+
+                    // MicroHIDOpeningDoorEventArgs ev = new(MicroHIDItem, DoorVariant, ValueOnStack);
+                    new(OpCodes.Newobj, GetDeclaredConstructors(typeof(MicroHIDOpeningDoorEventArgs))[0]),
+                    new(OpCodes.Dup),
+
+                    // Handlers.Player.OnMicroHIDOpeningDoor(ev);
+                    new(OpCodes.Call, Method(typeof(Handlers.Player), nameof(Handlers.Player.OnMicroHIDOpeningDoor))),
+
+                    // ev.IsAllowed (leave on stack)
+                    new(OpCodes.Callvirt, PropertyGetter(typeof(MicroHIDOpeningDoorEventArgs), nameof(MicroHIDOpeningDoorEventArgs.IsAllowed))),
+                });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
         }
     }
 }
