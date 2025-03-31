6f5e3c512a5ccd83494c80c92a500c534d5e36d7
Yamato <louismonneyron5@yahoo.com>
2025-03-31 16:50:53 +0200

    .

--- EXILED/Exiled.Events/Patches/Events/Player/ExplodingMicroHid.cs
+++ EXILED/Exiled.Events/Patches/Events/Player/ExplodingMicroHid.cs
@@ -9,22 +9,22 @@
 
 namespace Exiled.Events.Patches.Events.Player
 {
-    using Attributes;
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
 
+    using Exiled.API.Features.Pools;
+    using Exiled.Events.Attributes;
     using Exiled.Events.EventArgs.Player;
-
     using Footprinting;
     using HarmonyLib;
-
     using Interactables;
     using Interactables.Interobjects.DoorUtils;
     using InventorySystem.Items.MicroHID.Modules;
     using MapGeneration;
-
     using PlayerRoles.FirstPersonControl;
     using PlayerStatsSystem;
 
-    using UnityEngine;
+    using static HarmonyLib.AccessTools;
 
     /// <summary>
     /// Patches <see cref="InventorySystem.Items.MicroHID.Modules.ChargeFireModeModule.ServerExplode"/>.
@@ -34,6 +34,47 @@ namespace Exiled.Events.Patches.Events.Player
     [HarmonyPatch(typeof(ChargeFireModeModule), nameof(ChargeFireModeModule.ServerExplode))]
     internal static class ExplodingMicroHid
     {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+
+            int offset = -2;
+            int index = newInstructions.FindIndex(x => x.StoresField(Field(typeof(ChargeFireModeModule), nameof(ChargeFireModeModule._alreadyExploded)))) + offset;
+
+            Label retLabel = generator.DefineLabel();
+
+            newInstructions[newInstructions.Count - 1].labels.Add(retLabel);
+
+            newInstructions.InsertRange(
+                index,
+                new[]
+                {
+                    // this.MicroHid;
+                    new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(newInstructions[index]),
+                    new(OpCodes.Call, PropertyGetter(typeof(ChargeFireModeModule), nameof(ChargeFireModeModule.MicroHid))),
+
+                    // true
+                    new(OpCodes.Ldc_I4_1),
+
+                    // ExplodingMicroHIDEventArgs ev = new(MicroHIDItem, true);
+                    new(OpCodes.Newobj, GetDeclaredConstructors(typeof(ExplodingMicroHIDEventArgs))[0]),
+                    new(OpCodes.Dup),
+
+                    // Handlers.Player.OnExplodingMicroHID(ev);
+                    new(OpCodes.Call, Method(typeof(Handlers.Player), nameof(Handlers.Player.OnExplodingMicroHID))),
+
+                    // if (!ev.IsAllowed)
+                    //    return;
+                    new(OpCodes.Callvirt, PropertyGetter(typeof(ExplodingMicroHIDEventArgs), nameof(ExplodingMicroHIDEventArgs.IsAllowed))),
+                    new(OpCodes.Brfalse_S, retLabel),
+                });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+
         private static bool Prefix(ref ChargeFireModeModule __instance)
         {
             if (__instance._alreadyExploded)
