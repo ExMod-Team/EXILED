3197f26c6e4db1dd0fa679b0a2f6243b1640045c
MS-crew <100300664+MS-crew@users.noreply.github.com>
2025-04-09 13:06:01 +0300

    Update FinishingSense.cs
    Delete whitespaces

--- EXILED/Exiled.Events/Patches/Events/Scp049/FinishingSense.cs
+++ EXILED/Exiled.Events/Patches/Events/Scp049/FinishingSense.cs
@@ -10,18 +10,18 @@ namespace Exiled.Events.Patches.Events.Scp049
     using System.Collections.Generic;
     using System.Reflection;
     using System.Reflection.Emit;
-    
+
     using API.Features;
     using API.Features.Pools;
     using Exiled.Events.Attributes;
     using Exiled.Events.EventArgs.Scp049;
     using HarmonyLib;
-    
+
     using PlayerRoles.PlayableScps.Scp049;
     using PlayerRoles.Subroutines;
-    
+
     using static HarmonyLib.AccessTools;
-    
+
     /// <summary>
     /// Patches <see cref="Scp049SenseAbility.ServerLoseTarget" />.
     /// Adds the <see cref="Handlers.Scp049.FinishingSense" /> event.
