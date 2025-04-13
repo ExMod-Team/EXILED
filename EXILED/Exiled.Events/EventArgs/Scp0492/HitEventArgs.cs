19badbee46c0685dffaf5340f24eda4ca1e9a257
maro <zarnyarn@gmail.com>
2025-04-13 17:59:28 -0400

    HitEventArgs for SCP049-2

---
+++ EXILED/Exiled.Events/EventArgs/Scp0492/HitEventArgs.cs
@@ -0,0 +1,55 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HitEventArgs.cs" company="ExMod Team">
+// Copyright (c) ExMod Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.EventArgs.Scp0492
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using API.Features;
+    using Exiled.API.Features.Roles;
+    using Interfaces;
+    using PlayerRoles.PlayableScps.Subroutines;
+
+    /// <summary>
+    /// Contains all information after zombie sends an attack.
+    /// </summary>
+    public class HitEventArgs : IScp0492Event
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HitEventArgs"/> class.
+        /// </summary>
+        /// <param name="player"> <inheritdoc cref="Player"/></param>
+        /// <param name="result"> the result of the attack.</param>
+        /// <param name="playerHits"> the list of players who are getting hit.</param>
+        public HitEventArgs(ReferenceHub player, AttackResult result, HashSet<ReferenceHub> playerHits)
+        {
+            Player = Player.Get(player);
+            Scp0492 = Player.Role.As<Scp0492Role>();
+            Result = result;
+            PlayersAffected = playerHits.Select(Player.Get).ToList().AsReadOnly();
+        }
+
+        /// <summary>
+        /// Gets the player who is controlling SCP-049-2.
+        /// </summary>
+        public Player Player { get; }
+
+        /// <inheritdoc />
+        public Scp0492Role Scp0492 { get; }
+
+        /// <summary>
+        /// Gets the attack result for the server.
+        /// </summary>
+        public AttackResult Result { get; }
+
+        /// <summary>
+        /// Gets the attack result for the server.
+        /// </summary>
+        public IReadOnlyCollection<Player> PlayersAffected { get; }
+    }
+}
---
+++ EXILED/Exiled.Events/Patches/Events/Scp0492/Hit.cs
@@ -0,0 +1,71 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Hit.cs" company="ExMod Team">
+// Copyright (c) ExMod Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Patches.Events.Scp0492
+{
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
+    using API.Features.Pools;
+    using Attributes;
+    using Exiled.Events.EventArgs.Scp0492;
+    using HarmonyLib;
+    using PlayerRoles.PlayableScps.Scp049.Zombies;
+    using PlayerRoles.PlayableScps.Subroutines;
+    using PlayerRoles.Subroutines;
+
+    using static HarmonyLib.AccessTools;
+
+    /// <summary>
+    /// Patches ScpAttackAbilityBase.ServerPerformAttack
+    /// to add <see cref="Handlers.Scp0492.Hit" /> event.
+    /// </summary>
+    [EventPatch(typeof(Handlers.Scp0492), nameof(Handlers.Scp0492.Hit))]
+    [HarmonyPatch(typeof(ScpAttackAbilityBase<ZombieRole>), nameof(ScpAttackAbilityBase<ZombieRole>.ServerPerformAttack))]
+    public class Hit
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+
+            LocalBuilder ev = generator.DeclareLocal(typeof(HitEventArgs));
+
+            int offset = -1;
+            int index = newInstructions.FindLastIndex(x => x.opcode == OpCodes.Ret) + offset;
+
+            newInstructions.InsertRange(
+                index,
+                new[]
+                {
+                    // base.Owner
+                    new CodeInstruction(OpCodes.Ldarg_0),
+                    new(OpCodes.Call, PropertyGetter(typeof(StandardSubroutine<ZombieRole>), nameof(StandardSubroutine<ZombieRole>.Owner))),
+
+                    // this.LastAttackResult
+                    new CodeInstruction(OpCodes.Ldarg_0),
+                    new(OpCodes.Call, PropertyGetter(typeof(ScpAttackAbilityBase<ZombieRole>), nameof(ScpAttackAbilityBase<ZombieRole>.LastAttackResult))),
+
+                    // this.DetectedPlayers
+                    new CodeInstruction(OpCodes.Ldarg_0),
+                    new(OpCodes.Ldfld, Field(typeof(ScpAttackAbilityBase<ZombieRole>), nameof(ScpAttackAbilityBase<ZombieRole>.DetectedPlayers))),
+
+                    // HitEventArgs ev = new(ReferenceHub, AttackResult, HashSet<ReferenceHub>)
+                    new(OpCodes.Newobj, GetDeclaredConstructors(typeof(HitEventArgs))[0]),
+                    new(OpCodes.Dup),
+                    new(OpCodes.Stloc_S, ev.LocalIndex),
+
+                    // Handlers.Scp049.OnHit(ev)
+                    new(OpCodes.Call, Method(typeof(Handlers.Scp0492), nameof(Handlers.Scp0492.OnHit))),
+                });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+    }
+}
