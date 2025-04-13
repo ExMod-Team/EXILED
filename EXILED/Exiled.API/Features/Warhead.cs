918a9fd1ca027ec1336d83456508032ac33936de
GoldenPig1205 <78902671+GoldenPig1205@users.noreply.github.com>
2025-04-13 22:12:11 +0900

    Fix code where the default value of player value in player.ReferenceHub is null

--- EXILED/Exiled.API/Features/Warhead.cs
+++ EXILED/Exiled.API/Features/Warhead.cs
@@ -196,7 +196,7 @@ namespace Exiled.API.Features
         /// The <see cref="Player"/> who is disabling the warhead.
         /// If <see langword="null"/>, the warhead will be stopped without a specific player reference.
         /// </param>
-        public static void Stop(Player disabler = null) => Controller.CancelDetonation(disabler.ReferenceHub);
+        public static void Stop(ReferenceHub disabler = null) => Controller.CancelDetonation(disabler);
 
         /// <summary>
         /// Detonates the warhead after the specified remaining time.
