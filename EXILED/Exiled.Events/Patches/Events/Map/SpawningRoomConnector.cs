3770873d6a5c2cb3ac473fd101414ecab4e4b4a2
Yamato <louismonneyron5@yahoo.com>
2025-04-20 16:31:59 +0200

    Adding Room Propperty & Help please
    Can Someone help me to remove the Error on Ret ????

--- EXILED/Exiled.Events/Patches/Events/Map/SpawningRoomConnector.cs
+++ EXILED/Exiled.Events/Patches/Events/Map/SpawningRoomConnector.cs
@@ -1,4 +1,4 @@
-﻿// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
 // <copyright file="SpawningRoomConnector.cs" company="ExMod Team">
 // Copyright (c) ExMod Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
@@ -7,59 +7,94 @@
 
 namespace Exiled.Events.Patches.Events.Map
 {
+#pragma warning disable SA1402 // File may only contain a single type
+
     using System.Collections.Generic;
+    using System.Reflection;
     using System.Reflection.Emit;
 
+    using Exiled.API.Features;
     using Exiled.API.Features.Pools;
     using Exiled.Events.Attributes;
     using Exiled.Events.EventArgs.Map;
     using HarmonyLib;
-    using MapGeneration.RoomConnectors;
-    using Respawning;
+    using MapGeneration;
+    using MapGeneration.RoomConnectors.Spawners;
 
     using static HarmonyLib.AccessTools;
 
     /// <summary>
-    /// Patches <see cref="RoomConnectorDistributorSettings.TryGetTemplate"/>.
+    /// Patches <see cref="RoomConnectorSpawnpointBase.Spawn"/>.
     /// Adds the <see cref="Handlers.Map.OnSpawningRoomConnector"/> event.
     /// </summary>
     [EventPatch(typeof(Handlers.Map), nameof(Handlers.Map.SpawningRoomConnector))]
-    [HarmonyPatch(typeof(RoomConnectorDistributorSettings), nameof(RoomConnectorDistributorSettings.TryGetTemplate))]
+    [HarmonyPatch(typeof(RoomConnectorSpawnpointBase), nameof(RoomConnectorSpawnpointBase.Spawn))]
     internal static class SpawningRoomConnector
     {
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
             List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
-            LocalBuilder ev = generator.DeclareLocal(typeof(SpawningRoomConnectorEventArgs));
 
             Label retLabel = generator.DefineLabel();
 
-            newInstructions.InsertRange(0, new[]
+            newInstructions.InsertRange(0, new CodeInstruction[]
             {
-                // type
-                new CodeInstruction(OpCodes.Ldarg_0),
+                // this
+                new(OpCodes.Ldarg_0),
 
-                // SpawningRoomConnectorEventArgs ev = new SpawningRoomConnectorEventArgs(type)
-                new CodeInstruction(OpCodes.Newobj, AccessTools.Constructor(typeof(SpawningRoomConnectorEventArgs), new[] { typeof(SpawnableRoomConnectorType) })),
-                new CodeInstruction(OpCodes.Dup),
-                new CodeInstruction(OpCodes.Stloc, ev.LocalIndex),
+                // type
+                new(OpCodes.Ldarg_1),
+
+                // SpawningRoomConnectorEventArgs ev = new SpawningRoomConnectorEventArgs(RoomConnectorSpawnpointBase, SpawnableRoomConnectorType)
+                new(OpCodes.Newobj, Constructor(typeof(SpawningRoomConnectorEventArgs))),
+                new(OpCodes.Dup),
+                new(OpCodes.Dup),
 
                 // Handlers.Map.OnSpawningRoomConnector(ev);
-                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Handlers.Map), nameof(Handlers.Map.OnSpawningRoomConnector))),
+                new(OpCodes.Call, Method(typeof(Handlers.Map), nameof(Handlers.Map.OnSpawningRoomConnector))),
+
+                // type = ev.ConnectorType
+                new(OpCodes.Callvirt, PropertyGetter(typeof(SpawningRoomConnectorEventArgs), nameof(SpawningRoomConnectorEventArgs.ConnectorType))),
+                new(OpCodes.Starg_S, 1),
 
                 // if (!ev.IsAllowed)
                 //    return;
-                new CodeInstruction(OpCodes.Ldloc, ev.LocalIndex),
-                new CodeInstruction(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(SpawningRoomConnectorEventArgs), nameof(SpawningRoomConnectorEventArgs.IsAllowed))),
-                new CodeInstruction(OpCodes.Brfalse_S, retLabel),
-
-                // type = ev.ConnectorType
-                new CodeInstruction(OpCodes.Ldloc, ev.LocalIndex),
-                new CodeInstruction(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(SpawningRoomConnectorEventArgs), nameof(SpawningRoomConnectorEventArgs.ConnectorType))),
-                new CodeInstruction(OpCodes.Starg_S, 0),
+                new(OpCodes.Callvirt, PropertyGetter(typeof(SpawningRoomConnectorEventArgs), nameof(SpawningRoomConnectorEventArgs.IsAllowed))),
+                new(OpCodes.Brfalse_S, retLabel),
             });
 
             newInstructions[newInstructions.Count - 1].WithLabels(retLabel);
+
+            for (int z = 0; z < newInstructions.Count; z++)
+            {
+                Log.Info($"[{z}]{newInstructions[z].opcode}  {newInstructions[z].operand} ({newInstructions[z].labels.Count})");
+                yield return newInstructions[z];
+            }
+
+            ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+    }
+
+    /// <summary>
+    /// Patches <see cref="SeedSynchronizer.GenerateLevel"/>.
+    /// Adds the <see cref="Handlers.Map.OnSpawningRoomConnector"/> event.
+    /// </summary>
+    [EventPatch(typeof(Handlers.Map), nameof(Handlers.Map.SpawningRoomConnector))]
+    [HarmonyPatch(typeof(SeedSynchronizer), nameof(SeedSynchronizer.GenerateLevel))]
+    internal static class SpawningRoomConnectorFix
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+
+            int offset = 0;
+            int index = newInstructions.FindIndex(i => i.Calls(Method(typeof(RoomConnectorSpawnpointBase), nameof(RoomConnectorSpawnpointBase.SetupAllRoomConnectors)))) + offset;
+
+            newInstructions.RemoveAt(index);
+
+            offset = -1;
+            index = newInstructions.FindIndex(i => i.opcode == OpCodes.Newobj && (ConstructorInfo)i.operand == GetDeclaredConstructors(typeof(PluginAPI.Events.MapGeneratedEvent))[0]) + offset;
+            newInstructions.Insert(index, new CodeInstruction(OpCodes.Call, Method(typeof(RoomConnectorSpawnpointBase), nameof(RoomConnectorSpawnpointBase.SetupAllRoomConnectors))).MoveLabelsFrom(newInstructions[index]));
 
             for (int z = 0; z < newInstructions.Count; z++)
                 yield return newInstructions[z];
