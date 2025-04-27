f9d34097ecf572ab30f10a03ea840a156aeb3fb1
Bolton <48883340+BoltonDev@users.noreply.github.com>
2025-04-27 02:02:35 +0200

    fix: temp fix interacting door (need to refactor)

--- EXILED/Exiled.Events/Patches/Events/Player/InteractingDoor.cs
+++ EXILED/Exiled.Events/Patches/Events/Player/InteractingDoor.cs
@@ -7,27 +7,86 @@
 
 namespace Exiled.Events.Patches.Events.Player
 {
-    using System.Collections.Generic;
-    using System.Reflection.Emit;
-
-    using API.Features;
-    using Exiled.API.Features.Pools;
-    using Exiled.Events.Attributes;
-    using Exiled.Events.EventArgs.Player;
+    using Attributes;
+    using EventArgs.Player;
+    using Handlers;
     using HarmonyLib;
-
     using Interactables.Interobjects.DoorUtils;
-
-    using static HarmonyLib.AccessTools;
+    using LabApi.Events.Arguments.PlayerEvents;
+    using LabApi.Events.Handlers;
 
     /// <summary>
     /// Patches <see cref="DoorVariant.ServerInteract(ReferenceHub, byte)" />.
     /// Adds the <see cref="Handlers.Player.InteractingDoor" /> event.
     /// </summary>
-    [EventPatch(typeof(Handlers.Player), nameof(Handlers.Player.InteractingDoor))]
+    [EventPatch(typeof(Player), nameof(Player.InteractingDoor))]
     [HarmonyPatch(typeof(DoorVariant), nameof(DoorVariant.ServerInteract), typeof(ReferenceHub), typeof(byte))]
     internal static class InteractingDoor
     {
+#pragma warning disable SA1313
+        private static bool Prefix(DoorVariant __instance, ReferenceHub ply, byte colliderId)
+        {
+            PermissionUsed callback;
+            bool canOpen = __instance.RequiredPermissions.CheckPermissions(ply, __instance, out callback);
+            bool pluginRequestSent = false;
+
+            InteractingDoorEventArgs exiledEvent = new(API.Features.Player.Get(ply), __instance, colliderId, canOpen);
+            Exiled.Events.Handlers.Player.OnInteractingDoor(exiledEvent);
+
+            if (!exiledEvent.CanInteract)
+                return false;
+            canOpen = exiledEvent.IsAllowed;
+
+            if (__instance.ActiveLocks > 0 && !__instance.TryResolveLock(ply, out pluginRequestSent))
+            {
+                if (__instance._remainingDeniedCooldown <= 0.0)
+                {
+                    __instance._remainingDeniedCooldown = __instance.DeniedCooldown;
+                    __instance.LockBypassDenied(ply, colliderId);
+                    if (callback != null)
+                        callback(__instance, false);
+                }
+
+                PlayerEvents.OnInteractedDoor(new PlayerInteractedDoorEventArgs(ply, __instance, canOpen));
+            }
+            else
+            {
+                if (!__instance.AllowInteracting(ply, colliderId))
+                    return false;
+                if (!pluginRequestSent)
+                {
+                    PlayerInteractingDoorEventArgs ev = new(ply, __instance, canOpen);
+                    PlayerEvents.OnInteractingDoor(ev);
+                    if (!ev.IsAllowed)
+                        return false;
+                    canOpen = ev.CanOpen;
+                }
+
+                if (canOpen)
+                {
+                    __instance.NetworkTargetState = !__instance.TargetState;
+                    __instance._triggerPlayer = ply;
+                    if (__instance.DoorName != null)
+                        ServerLogs.AddLog(ServerLogs.Modules.Door, ply.LoggedNameFromRefHub() + " " + (__instance.TargetState ? "opened" : "closed") + " " + __instance.DoorName + ".", ServerLogs.ServerLogType.GameEvent);
+                    if (callback != null)
+                        callback(__instance, true);
+                }
+                else if (__instance._remainingDeniedCooldown <= 0.0)
+                {
+                    __instance._remainingDeniedCooldown = __instance.DeniedCooldown;
+                    __instance.PermissionsDenied(ply, colliderId);
+                    DoorEvents.TriggerAction(__instance, DoorAction.AccessDenied, ply);
+                    if (callback != null)
+                        callback(__instance, false);
+                }
+
+                PlayerEvents.OnInteractedDoor(new PlayerInteractedDoorEventArgs(ply, __instance, canOpen));
+            }
+
+            return false;
+        }
+
+        /*
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
             List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
@@ -41,7 +100,7 @@ namespace Exiled.Events.Patches.Events.Player
                 new(OpCodes.Ldarg_1),
                 new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
 
-                // this
+                // __instance
                 new(OpCodes.Ldarg_0),
 
                 // colliderId
@@ -62,11 +121,11 @@ namespace Exiled.Events.Patches.Events.Player
                 new(OpCodes.Call, Method(typeof(Handlers.Player), nameof(Handlers.Player.OnInteractingDoor))),
                 new(OpCodes.Stloc_S, ev.LocalIndex),
 
-                // if (!ev.CanInteract) return;
+                // if (!ev.CanInteract) return false;
                 new(OpCodes.Callvirt, PropertyGetter(typeof(InteractingDoorEventArgs), nameof(InteractingDoorEventArgs.CanInteract))),
                 new(OpCodes.Brfalse_S, retLabel),
 
-                // CanInteract = ev.IsAllowed (Reminder this is done on purpose because we prefer than IDeniableEvent when cancel use CanInteract and not Interacting)
+                // CanInteract = ev.IsAllowed (Reminder __instance is done on purpose because we prefer than IDeniableEvent when cancel use CanInteract and not Interacting)
                 new(OpCodes.Ldloc_S, ev.LocalIndex),
                 new(OpCodes.Callvirt, PropertyGetter(typeof(InteractingDoorEventArgs), nameof(InteractingDoorEventArgs.IsAllowed))),
                 new(OpCodes.Stloc_1),
@@ -86,6 +145,6 @@ namespace Exiled.Events.Patches.Events.Player
                 yield return newInstructions[z];
 
             ListPool<CodeInstruction>.Pool.Return(newInstructions);
-        }
+        }*/
     }
 }
