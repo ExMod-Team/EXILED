88112848031b8b9b8d51d65d588ccbe89ced8a75
MS-crew <100300664+MS-crew@users.noreply.github.com>
2025-08-31 18:12:45 +0300

    Update and rename VoiceChattingEventArgs.cs to SendingVoiceMessageEventArgs.cs

--- EXILED/Exiled.Events/EventArgs/Player/SendingVoiceMessageEventArgs.cs
+++ EXILED/Exiled.Events/EventArgs/Player/SendingVoiceMessageEventArgs.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="VoiceChattingEventArgs.cs" company="ExMod Team">
+// <copyright file="SendingVoiceMessageEventArgs.cs" company="ExMod Team">
 // Copyright (c) ExMod Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
@@ -9,41 +9,35 @@ namespace Exiled.Events.EventArgs.Player
 {
     using Exiled.API.Features;
     using Exiled.Events.EventArgs.Interfaces;
-
     using PlayerRoles.Voice;
-
     using VoiceChat.Networking;
 
+    using IVoiceRole = API.Features.Roles.IVoiceRole;
+
     /// <summary>
-    /// Contains all information after a player presses the voicechat key.
+    /// Contains all information before a player sending a voice message.
     /// </summary>
-    public class VoiceChattingEventArgs : IPlayerEvent, IDeniableEvent
+    public class SendingVoiceMessageEventArgs : IPlayerEvent, IDeniableEvent
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="VoiceChattingEventArgs" /> class.
+        /// Initializes a new instance of the <see cref="SendingVoiceMessageEventArgs" /> class.
         /// </summary>
-        /// <param name="player">
-        /// <inheritdoc cref="Player" />
-        /// </param>
         /// <param name="voiceMessage">
         /// <inheritdoc cref="VoiceMessage" />
         /// </param>
-        /// <param name="voiceModule">
-        /// <inheritdoc cref="VoiceModule" />
-        /// </param>
-        /// <param name="isAllowed">
-        /// <inheritdoc cref="IsAllowed" />
-        /// </param>
-        public VoiceChattingEventArgs(Player player, VoiceMessage voiceMessage, VoiceModuleBase voiceModule, bool isAllowed = true)
+        public SendingVoiceMessageEventArgs(VoiceMessage voiceMessage)
         {
-            Player = player;
+            Player = Player.Get(voiceMessage.Speaker);
             VoiceMessage = voiceMessage;
-            VoiceModule = voiceModule;
-            IsAllowed = isAllowed;
+
+            if (Player.Role is IVoiceRole iVR)
+            {
+                VoiceModule = iVR.VoiceModule;
+            }
         }
 
         /// <summary>
-        /// Gets the player who's voicechatting.
+        /// Gets the player who's sending voice message.
         /// </summary>
         public Player Player { get; }
 
@@ -58,8 +52,8 @@ namespace Exiled.Events.EventArgs.Player
         public VoiceModuleBase VoiceModule { get; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether the player can voicechat.
+        /// Gets or sets a value indicating whether the player can send this voice message.
         /// </summary>
-        public bool IsAllowed { get; set; }
+        public bool IsAllowed { get; set; } = true;
     }
 }
