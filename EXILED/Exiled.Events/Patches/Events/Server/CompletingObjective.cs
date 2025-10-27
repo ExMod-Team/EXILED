4ed9ef13fdbe8975780ec3c75aeb1f389a99c08d
Yamato <66829532+louis1706@users.noreply.github.com>
2025-10-27 18:26:32 +0100

    CompletingObjective update

--- EXILED/Exiled.Events/Patches/Events/Server/CompletingObjective.cs
+++ EXILED/Exiled.Events/Patches/Events/Server/CompletingObjective.cs
@@ -1,4 +1,4 @@
-﻿// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
 // <copyright file="CompletingObjective.cs" company="ExMod Team">
 // Copyright (c) ExMod Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
@@ -8,13 +8,19 @@
 namespace Exiled.Events.Patches.Events.Server
 {
     using System.Collections.Generic;
+    using System.Linq;
     using System.Reflection.Emit;
 
     using Exiled.API.Features.Pools;
     using Exiled.Events.Attributes;
+    using Exiled.Events.EventArgs.Map;
     using Exiled.Events.EventArgs.Server;
+    using Exiled.Events.Patches.Events.Map;
     using HarmonyLib;
+    using PlayerRoles;
+    using Respawning;
     using Respawning.Objectives;
+    using Respawning.Waves;
 
     using static HarmonyLib.AccessTools;
 
@@ -30,7 +36,7 @@ namespace Exiled.Events.Patches.Events.Server
         {
             List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
 
-            Label returnLabel = generator.DefineLabel();
+            Label retModLabel = generator.DefineLabel();
 
             newInstructions.InsertRange(0, new CodeInstruction[]
             {
@@ -48,17 +54,37 @@ namespace Exiled.Events.Patches.Events.Server
                 new(OpCodes.Call, Method(typeof(Handlers.Server), nameof(Handlers.Server.OnCompletingObjective))),
 
                 // if (!ev.IsAllowed)
-                //     return;
-                new(OpCodes.Callvirt, PropertyGetter(typeof(CompletingObjectiveEventArgs), nameof(CompletingObjectiveEventArgs.IsAllowed))),
-                new(OpCodes.Brfalse_S, returnLabel),
+                //    return;
+                new(OpCodes.Callvirt, PropertyGetter(typeof(ObjectiveCompletingEventArgs), nameof(ObjectiveCompletingEventArgs.IsAllowed))),
+                new(OpCodes.Brfalse_S, retModLabel),
             });
 
-            newInstructions[newInstructions.Count - 1].labels.Add(returnLabel);
+            newInstructions.InsertRange(newInstructions.Count, new CodeInstruction[]
+            {
+                // faction
+                new CodeInstruction(OpCodes.Ldarg_0).WithLabels(retModLabel),
+                new (OpCodes.Call, Method(typeof(CompletingObjective), nameof(RevertAllValue))),
+            });
 
             for (int z = 0; z < newInstructions.Count; z++)
                 yield return newInstructions[z];
 
             ListPool<CodeInstruction>.Pool.Return(newInstructions);
+        }
+
+        private static void RevertAllValue(FactionObjectiveBase factionObjectiveBase)
+        {
+            if (factionObjectiveBase is IFootprintObjective footprintObjective)
+            {
+                FactionInfluenceManager.Remove(footprintObjective.ObjectiveFootprint.AchievingPlayer.RoleType.GetFaction(), -footprintObjective.ObjectiveFootprint.InfluenceReward);
+                foreach (TimeBasedWave wave in WaveManager.Waves.Cast<TimeBasedWave>())
+                {
+                    if (wave.TargetFaction == footprintObjective.ObjectiveFootprint.AchievingPlayer.RoleType.GetFaction() && wave.ReceiveObjectiveRewards)
+                    {
+                        wave.Timer.AddTime(-footprintObjective.ObjectiveFootprint.TimeReward);
+                    }
+                }
+            }
         }
     }
 }
