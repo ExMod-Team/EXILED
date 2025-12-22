46af72aa7cd44c7399c8945796a4a55c22317d06
michcio <michcia.179@gmail.com>
2025-12-22 20:53:01 +0100

    Moved to Player Handler

--- EXILED/Exiled.Events/EventArgs/Player/Scp1576TransmissionEndedEventArgs.cs
+++ EXILED/Exiled.Events/EventArgs/Player/Scp1576TransmissionEndedEventArgs.cs
@@ -1,11 +1,11 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="TransmissionEndedEventArgs.cs" company="ExMod Team">
+// <copyright file="Scp1576TransmissionEndedEventArgs.cs" company="ExMod Team">
 // Copyright (c) ExMod Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
 // -----------------------------------------------------------------------
 
-namespace Exiled.Events.EventArgs.Scp1576
+namespace Exiled.Events.EventArgs.Player
 {
     using API.Features;
     using API.Features.Items;
@@ -15,14 +15,14 @@ namespace Exiled.Events.EventArgs.Scp1576
     /// <summary>
     /// Contains all information after a SCP-1576 transmission has ended.
     /// </summary>
-    public class TransmissionEndedEventArgs : IPlayerEvent
+    public class Scp1576TransmissionEndedEventArgs : IPlayerEvent
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="TransmissionEndedEventArgs"/> class.
+        /// Initializes a new instance of the <see cref="Scp1576TransmissionEndedEventArgs"/> class.
         /// </summary>
         /// <param name="player"><inheritdoc cref="Player"/></param>
         /// <param name="scp1576Item"><inheritdoc cref="Scp1576"/></param>
-        public TransmissionEndedEventArgs(Player player, Scp1576Item scp1576Item)
+        public Scp1576TransmissionEndedEventArgs(Player player, Scp1576Item scp1576Item)
         {
             Player = player;
             Scp1576 = Item.Get<Scp1576>(scp1576Item);
--- EXILED/Exiled.Events/Patches/Events/Player/Scp1576TransmissionEnded.cs
+++ EXILED/Exiled.Events/Patches/Events/Player/Scp1576TransmissionEnded.cs
@@ -1,30 +1,29 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="TransmissionEnded.cs" company="ExMod Team">
+// <copyright file="Scp1576TransmissionEnded.cs" company="ExMod Team">
 // Copyright (c) ExMod Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
 // -----------------------------------------------------------------------
 
-namespace Exiled.Events.Patches.Events.Scp1576
+namespace Exiled.Events.Patches.Events.Player
 {
     using System.Collections.Generic;
     using System.Reflection.Emit;
 
-    using Exiled.API.Features;
     using Exiled.API.Features.Pools;
     using Exiled.Events.Attributes;
-    using Exiled.Events.EventArgs.Scp1576;
+    using Exiled.Events.EventArgs.Player;
     using HarmonyLib;
     using InventorySystem.Items.Usables.Scp1576;
 
     using static HarmonyLib.AccessTools;
 
     /// <summary>
-    /// Patches <see cref="Scp1576Item.ServerStopTransmitting"/> to add <see cref="Handlers.Scp1576.TransmissionEnded"/> event.
+    /// Patches <see cref="Scp1576Item.ServerStopTransmitting"/> to add <see cref="Handlers.Player.Scp1576TransmissionEnded"/> event.
     /// </summary>
-    [EventPatch(typeof(Handlers.Scp1576), nameof(Handlers.Scp1576.TransmissionEnded))]
+    [EventPatch(typeof(Handlers.Player), nameof(Handlers.Player.Scp1576TransmissionEnded))]
     [HarmonyPatch(typeof(InventorySystem.Items.Usables.Scp1576.Scp1576Item), nameof(Scp1576Item.ServerStopTransmitting))]
-    public class TransmissionEnded
+    public class Scp1576TransmissionEnded
     {
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
@@ -39,16 +38,16 @@ namespace Exiled.Events.Patches.Events.Scp1576
                         // Player.Get(base.Owner)
                         new CodeInstruction(OpCodes.Ldarg_0),
                         new CodeInstruction(OpCodes.Callvirt, PropertyGetter(typeof(Scp1576Item), nameof(Scp1576Item.Owner))),
-                        new CodeInstruction(OpCodes.Call, Method(typeof(API.Features.Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
+                        new CodeInstruction(OpCodes.Call, Method(typeof(API.Features.Player), nameof(API.Features.Player.Get), new[] { typeof(ReferenceHub) })),
 
                         // this
                         new CodeInstruction(OpCodes.Ldarg_0),
 
                         // ServerStopTransmittingEventArgs ev = new(Player, this)
-                        new CodeInstruction(OpCodes.Newobj, GetDeclaredConstructors(typeof(TransmissionEndedEventArgs))[0]),
+                        new CodeInstruction(OpCodes.Newobj, GetDeclaredConstructors(typeof(Scp1576TransmissionEndedEventArgs))[0]),
 
                         // OnTransmisionEnded(ev)
-                        new CodeInstruction(OpCodes.Call, Method(typeof(Handlers.Scp1576), nameof(Handlers.Scp1576.OnTransmisionEnded))),
+                        new CodeInstruction(OpCodes.Call, Method(typeof(Handlers.Player), nameof(Handlers.Player.OnScp1576TransmissionEnded))),
                     });
 
                 for (int i = 0; i < newInstructions.Count; i++)
