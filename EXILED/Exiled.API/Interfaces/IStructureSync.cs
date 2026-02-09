b7fdda308949d073a9f8935c402b254c55eb54de
@Someone <45270312+Someone-193@users.noreply.github.com>
2026-02-09 04:38:58 -0500

    feat: Utilize StructurePositionSync to fix and add setters to structure positions and rotations (#745)
    * generator + interface
    
    * Better docs, add locker + workstation
    
    ---------
    
    Co-authored-by: Yamato <66829532+louis1706@users.noreply.github.com>

---
+++ EXILED/Exiled.API/Interfaces/IStructureSync.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IStructureSync.cs" company="ExMod Team">
+// Copyright (c) ExMod Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.API.Interfaces
+{
+    using MapGeneration.Distributors;
+
+    /// <summary>
+    /// Represents an object with a <see cref="StructurePositionSync"/>.
+    /// </summary>
+    public interface IStructureSync
+    {
+        /// <summary>
+        /// Gets the <see cref="StructurePositionSync"/> of this structure.
+        /// </summary>
+        public StructurePositionSync PositionSync { get; }
+    }
+}
