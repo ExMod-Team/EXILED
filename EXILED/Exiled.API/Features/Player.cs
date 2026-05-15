a20bd5606d00958efc6d08ac4279ba97cf432382
VALERA771 <val.germanov@gmail.com>
2026-05-15 17:58:05 +0300

    my client pleads oopsie-doopsie

--- EXILED/Exiled.API/Features/Player.cs
+++ EXILED/Exiled.API/Features/Player.cs
@@ -5,12 +5,11 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
-using System.Diagnostics.CodeAnalysis;
-
 namespace Exiled.API.Features
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
     using System.Linq;
     using System.Reflection;
     using System.Runtime.CompilerServices;
