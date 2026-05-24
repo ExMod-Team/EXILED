cea316e92156b90b138b20321dd788c2d922b12f
Mustafa SAVAŞ <mustafavesavas1@gmail.com>
2026-05-24 18:05:59 +0300

    remove unused usings

--- EXILED/Exiled.Events/Patches/Events/Player/Spawning.cs
+++ EXILED/Exiled.Events/Patches/Events/Player/Spawning.cs
@@ -8,12 +8,10 @@
 namespace Exiled.Events.Patches.Events.Player
 {
     using System.Collections.Generic;
-    using System.Reflection;
     using System.Reflection.Emit;
 
     using Exiled.API.Features;
     using Exiled.API.Features.Pools;
-    using Exiled.API.Features.Roles;
     using Exiled.Events.Attributes;
     using Exiled.Events.EventArgs.Player;
 
