6b5dae27fe987a989b09c838ae01d794fc4e70fc
Yamato <66829532+louis1706@users.noreply.github.com>
2026-06-21 23:17:05 +0200

    fix: Lift & Pickup Get(GameObject)

--- EXILED/Exiled.API/Features/Lift.cs
+++ EXILED/Exiled.API/Features/Lift.cs
@@ -257,7 +257,7 @@ namespace Exiled.API.Features
         /// </summary>
         /// <param name="gameObject">The <see cref="UnityEngine.GameObject"/>.</param>
         /// <returns>A <see cref="Lift"/> or <see langword="null"/> if not found.</returns>
-        public static Lift Get(GameObject gameObject) => Get(lift => lift.GameObject == gameObject).FirstOrDefault();
+        public static Lift Get(GameObject gameObject) => Get(gameObject.GetComponentInParent<ElevatorChamber>(false));
 
         /// <summary>
         /// Gets the <see cref="Lift"/> belonging to the <see cref="Vector3"/>, if any.
