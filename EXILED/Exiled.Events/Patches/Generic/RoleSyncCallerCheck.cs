d367f5ac3376c8c7abf8a7eba8c006ec41ecc973
Yamato <66829532+louis1706@users.noreply.github.com>
2026-07-16 17:08:12 +0200

    perf: no Transpiller when not needed

--- EXILED/Exiled.Events/Patches/Generic/RoleSyncCallerCheck.cs
+++ EXILED/Exiled.Events/Patches/Generic/RoleSyncCallerCheck.cs
@@ -1,4 +1,4 @@
-﻿// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
 // <copyright file="RoleSyncCallerCheck.cs" company="ExMod Team">
 // Copyright (c) ExMod Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
@@ -23,24 +23,8 @@ namespace Exiled.Events.Patches.Generic
     [HarmonyPatch(typeof(PlayerRoleManager), nameof(PlayerRoleManager.SendNewRoleInfo))]
     internal class RoleSyncCallerCheck
     {
-        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Pool.Get(instructions);
+        private static void Prefix() => Round.SendingNewRoleInfo = true;
 
-            yield return new CodeInstruction(OpCodes.Ldc_I4_1);
-            yield return new CodeInstruction(OpCodes.Call, PropertySetter(typeof(Round), nameof(Round.SendingNewRoleInfo)));
-
-            int z;
-            for (z = 0; z < newInstructions.Count - 1; z++)
-                yield return newInstructions[z];
-            CodeInstruction ret = newInstructions[z];
-
-            yield return new CodeInstruction(OpCodes.Ldc_I4_0).MoveLabelsFrom(ret);
-            yield return new CodeInstruction(OpCodes.Call, PropertySetter(typeof(Round), nameof(Round.SendingNewRoleInfo)));
-
-            yield return ret;
-
-            ListPool<CodeInstruction>.Pool.Return(newInstructions);
-        }
+        private static void Postfix() => Round.SendingNewRoleInfo = false;
     }
 }
